891019da7720cebfa09d9a598fa63deb6452568d
fehuyngo <fe.huyngo@gmail.com>
2024-08-05 08:56:40 +0700

    .

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,22 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
   email: string;
+
+  @IsNotEmpty({
+    message: 'Password không được để trống',
+  })
   password: string;
+
   name: string;
-  //   phone: string;
-  //   age: number;
   address: string;
-  //   createdAt: Date;
-  //   updatedAt: Date;
 }
