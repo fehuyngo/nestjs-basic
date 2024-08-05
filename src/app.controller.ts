24fe02aa4fb3ff8f697bbd037bf75e16dbf808c6
fehuyngo <fe.huyngo@gmail.com>
2024-08-05 15:42:52 +0700

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,13 +10,9 @@ export class AppController {
     private configService: ConfigService,
   ) {}
 
-  @Get()
-  @Render('home')
-  handleHomePage() {
-    const message = this.appService.getHello();
-    return {
-      message,
-    };
-    // return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return req.user;
   }
 }
