662c77bbef1cf17183c18f35a9f2491ca2b666be
fehuyngo <fe.huyngo@gmail.com>
2024-08-07 09:44:47 +0700

    .

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
-import mongoose, { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import mongoose from 'mongoose';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-
+  constructor(
+    @InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>,
+  ) {}
   getHashPassword = (password: string) => {
     const salt = genSaltSync(10);
     const hash = hashSync(password, salt);
@@ -65,7 +67,7 @@ export class UsersService {
       return 'Not Found User';
     }
 
-    return this.userModel.deleteOne({
+    return this.userModel.softDelete({
       _id: id,
     });
   }
