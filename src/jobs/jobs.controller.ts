97050dceb92401acc58e72d4df17342c3d3c1a1b
fehuyngo <fe.huyngo@gmail.com>
2024-08-13 13:55:02 +0700

    .

---
+++ src/jobs/jobs.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
+
+@Controller('jobs')
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Post()
+  @ResponseMessage('Create a new job')
+  async create(@Body() createJobDto: CreateJobDto, @User() user: IUser) {
+    const newJob = await this.jobsService.create(createJobDto, user);
+    return {
+      _id: newJob?._id,
+      createdAt: newJob?.createdAt,
+    };
+  }
+
+  @Get()
+  @ResponseMessage('Fetch jobs with paginate')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() qs: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, qs);
+  }
+
+  @Public()
+  @Get(':id')
+  @ResponseMessage('Fetch a job by id')
+  findOne(@Param('id') id: string) {
+    return this.jobsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ResponseMessage('Update a job')
+  update(
+    @Param('id') id: string,
+    @Body() updateJobDto: UpdateJobDto,
+    @User() user: IUser,
+  ) {
+    return this.jobsService.update(id, updateJobDto, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
+  }
+}
---
+++ src/jobs/jobs.service.ts
@@ -0,0 +1,106 @@
+import { Injectable } from '@nestjs/common';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Job, JobDocument } from './schemas/job.schema';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params';
+
+@Injectable()
+export class JobsService {
+  constructor(
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>,
+  ) {}
+
+  create(createJobDto: CreateJobDto, user: IUser) {
+    return this.jobModel.create({
+      ...createJobDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+  }
+
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+
+    const offset = (+currentPage - 1) * +limit;
+    const defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.jobModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.jobModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
+  }
+
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'Not Found Job';
+    }
+
+    return this.jobModel.findOne({
+      _id: id,
+    });
+  }
+
+  async update(id: string, updateJobDto: UpdateJobDto, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'Not Found Job';
+    }
+
+    return await this.jobModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        ...updateJobDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+  }
+
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'Not Found Job';
+    }
+
+    await this.jobModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return await this.jobModel.softDelete({
+      _id: id,
+    });
+  }
+}
