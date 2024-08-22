d7e7439485a8dd0b738d3d0a0267218f893ae087
fehuyngo <fe.huyngo@gmail.com>
2024-08-22 15:29:00 +0700

    .

--- src/subscribers/subscribers.service.ts
+++ src/subscribers/subscribers.service.ts
@@ -77,14 +77,10 @@ export class SubscribersService {
     return await this.subscriberModel.findOne({ _id: id });
   }
 
-  async update(
-    id: string,
-    updateSubscriberDto: UpdateSubscriberDto,
-    user: IUser,
-  ) {
+  async update(updateSubscriberDto: UpdateSubscriberDto, user: IUser) {
     const updated = await this.subscriberModel.updateOne(
       {
-        _id: id,
+        email: user.email,
       },
       {
         ...updateSubscriberDto,
@@ -92,6 +88,9 @@ export class SubscribersService {
           _id: user._id,
           email: user.email,
         },
+      },
+      {
+        upsert: true,
       },
     );
     return updated;
@@ -116,5 +115,10 @@ export class SubscribersService {
     return await this.subscriberModel.softDelete({
       _id: id,
     });
+  }
+
+  async getSkills(user: IUser) {
+    const { email } = user;
+    return await this.subscriberModel.findOne({ email }, { skills: 1 });
   }
 }
